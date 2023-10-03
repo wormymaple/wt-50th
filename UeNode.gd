9bef5cad4de85418eadcbe0b4a4d13407f5bee27
KJwacky <kjwacky13@gmail.com>
2023-10-03 08:56:12 -0600

    NodeDoesn'tCenter

--- UeNode.gd
+++ UeNode.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var selected = false
+var myClickPos
+var myDistance = Vector2(0,0)
 
 func _ready():
 	pass 
@@ -10,10 +12,14 @@ func _process(delta):
 		followMouse()
 
 func followMouse():
-	position = get_global_mouse_position()
+	position = get_global_mouse_position()+myDistance
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		
+		myClickPos = get_global_mouse_position()
+		myDistance = get_position()-myClickPos
+		
 		if event.pressed:
 			selected = true
 		else:
