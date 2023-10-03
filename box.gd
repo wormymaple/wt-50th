2dd43c260a39dc8f33c7648e1b4734ef50d73685
Davianf <142517971+Davianf@users.noreply.github.com>
2023-10-03 10:24:30 -0600

    box tcsn

--- box.gd
+++ box.gd
@@ -1,10 +1,30 @@
-onready var box = preload('res://box.tscn')
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_box_input_event(viewport, event, shape_idx):
+	if	event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		print ('lol it works')
+		queue_free()
+#onready var box = preload("res://box.tscn")
+
 func _ready():
 	new_box()
+	
 func new_box():
-	var new_box = box.instance()
-	add_child(new_box)
-	if	event is inputeventmousebutton and event.pressed and event.button_index == BUTTON_LEFT:
-		print ("lol it works")
-		queue_free()
-_on_box_input_event (vieqport, event, shape_idx)
+	var New_box = box.instance()
+	add_child(New_box)
