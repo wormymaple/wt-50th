99ca19f1f4c4bf81111b735c400a01a886fd682e
Davianf <142517971+Davianf@users.noreply.github.com>
2023-10-04 07:58:05 -0600

    almost working
    i just need to add some code to make it so where it can work

--- box.gd
+++ box.gd
@@ -16,15 +16,28 @@ extends RigidBody2D
 #	pass
 
 
-func _on_box_input_event(viewport, event, shape_idx):
+func _on_box_input_event(_viewport, event, _shape_idx):
 	if	event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		print ('lol it works')
 		queue_free()
-#onready var box = preload("res://box.tscn")
+onready var box = load("res://box.tscn")
 
 func _ready():
 	new_box()
 	
 func new_box():
-	var New_box = box.instance()
-	add_child(New_box)
+	var new_box = box.instance()
+	add_child(new_box)
+func select():
+	yield(get_tree().create_timer(0.1),"timeout")
+	if not flagged and not selected:
+		selected = true 
+		if mie:
+			set_texture(mine_block)
+			
+			else:
+				var mines = cheack_neighbors()
+				match mines:
+					0:
+			set_texture(clear_blocked)
+			select_neighbors()
