202264a59685cdc509b36cebe9971bbb93a77110
Halll150 <2093846@jeffcoschools.us>
2023-10-04 10:59:23 -0600

    Heat gauge values work

--- Games/Welding/Heat_Button.gd
+++ Games/Welding/Heat_Button.gd
@@ -1,12 +1,25 @@
-extends Node2D
+extends Button
+
+var is_pressed = false 
+var heat: float 
+var max_or_min: bool 
 
 func _on_Button_button_down():
-	_set(_property, value):
+	is_pressed = true
+	heat += 1
 
 func _on_Button_button_up():
-	print("Up")
+	is_pressed = false
+	heat -= 1
 
-func _set(_property, value):
-	value = value + 1 
-
-func
+func _process(delta):
+	if heat <= 0 or heat >= 100:
+		max_or_min = true
+	elif heat > 0 and heat < 100:
+		max_or_min = false
+	if is_pressed == true and max_or_min == false: 
+		heat += 1
+		print(heat) 
+	elif is_pressed == false and max_or_min == false:
+		heat -= 1
+		print(heat)
