c745d09bcd70b513258791c11a3f940753278b7a
Halll150 <2093846@jeffcoschools.us>
2023-10-04 09:19:49 -0600

    Button added

---
+++ Games/Welding/Heat_Button.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _on_Button_button_down():
+	_set(_property, value):
+
+func _on_Button_button_up():
+	print("Up")
+
+func _set(_property, value):
+	value = value + 1 
+
+func
