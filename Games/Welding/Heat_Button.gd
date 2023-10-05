63bcdf0fc0e80f5f5f526ebcf3fd8df47e58944b
Halll150 <2093846@jeffcoschools.us>
2023-10-05 11:00:16 -0600

    Heat gauge

--- Games/Welding/Heat_Button.gd
+++ Games/Welding/Heat_Button.gd
@@ -3,6 +3,11 @@ extends Button
 var is_pressed = false 
 var heat: float 
 var max_or_min: bool 
+var score: int
+export var color: Gradient
+
+func _on_Heat_Bar_value_changed(_value):
+	$Heat_Bar.color = color.interpolate(heat)
 
 func _on_Button_button_down():
 	is_pressed = true
@@ -12,7 +17,7 @@ func _on_Button_button_up():
 	is_pressed = false
 	heat -= 1
 
-func _process(delta):
+func _process(_delta):
 	if heat <= 0 or heat >= 100:
 		max_or_min = true
 	elif heat > 0 and heat < 100:
@@ -22,3 +27,6 @@ func _process(delta):
 	elif is_pressed == false and max_or_min == false:
 		heat -= 1
 	$Heat_Bar.value = heat 
+	if heat < 75 and heat > 25:
+		score += 1
+		print(score)
---
+++ Games/Welding/WeldingGun.gd
@@ -0,0 +1,20 @@
+extends Sprite
+
+export var move_curve: Curve
+export var move_pos: float
+export var move_time_max: float
+var move_time = 0.0
+var start_y
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_y = global_position.y
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	move_time += delta
+	if move_time > move_time_max:
+		move_time -= move_time_max
+	
+	global_position = Vector2(global_position.x, move_curve.interpolate(move_time / move_time_max) * move_pos + start_y)
