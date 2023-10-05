c7945a2d323dbfd467ad23e9138a53bd5d4f6380
Hiro Kahara <hirokahara@icloud.com>
2023-10-05 09:46:02 -0600

    Menu + Credits
    Added these in from our projects and am still fixing change scene paths but it works.

---
+++ Game Assets/UserInterface/Title screen.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://Game Assets/UserInterface/Level 2.tscn")
+
+func _on_Quit_pressed():
+	get_tree().quit
+
+
+func _on_Credits_pressed():
+	get_tree().change_scene("res://Game Assets/UserInterface/GodotCredits.tscn")
