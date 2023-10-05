8e47c63953fd0ce8395621bb0dc1c5fee7d319ec
Oochiiehehe3 <142919558+Oochiiehehe3@users.noreply.github.com>
2023-10-05 09:30:12 -0600

    made the dot check if it was in the green area

--- Scenes/SpriteRotateFaster.gd
+++ Scenes/SpriteRotateFaster.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export (float) var rotation_speed = -6
 export (bool) var is_rotating = true
+export (bool) var dot_in_range = true
 
 #func get_input():
 	#rotation_dir = 0
@@ -20,4 +21,14 @@ func _physics_process(delta):
 
 func _on_Button_button_down():
 	is_rotating = false
+	if (dot_in_range == true):
+		print("hooray")
 	# print("stop pls?")
+
+
+func _on_Area2D_area_entered(area):
+	dot_in_range = true
+
+
+func _on_Area2D_area_exited(area):
+	dot_in_range = false
