6fe9bde36e2228741da3e008d9df10ab0922340f
BeckhamSutton <142523638+BeckhamSutton@users.noreply.github.com>
2023-10-06 07:53:04 -0600

    Random

--- Sprite.gd
+++ Sprite.gd
@@ -14,3 +14,31 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+var label: String
+var dropped_on_target: bool = false
+
+
+
+func _ready():
+	add_to_group("DRAGGABLE")
+	pass
+
+func get_drag_data(position: Vector2):
+	var slot = get_parent().get_name()
+
+	var data = {}
+	data["origin_node"] = self
+	data["origin_slot"] = slot
+	data["origin_texture_normal"] = texture_normal
+	data["origin_texture_pressed"] = texture_pressed
+	
+	var dragPreview = DRAGPREVIEW.instance()
+	dragPreview.texture = texture_normal
+	add_child(dragPreview)
+
+	return data
+
+
+
