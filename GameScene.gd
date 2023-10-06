d75b313535e24faedddbebfa20daf1555a561f8a
Hiro Kahara <hirokahara@icloud.com>
2023-10-06 10:59:18 -0600

    Made progress bar work
    Scene is almost ready to instantantiate other scenes

--- GameScene.gd
+++ GameScene.gd
@@ -2,22 +2,31 @@ extends Control
 
 onready var GameTimer = $Timer
 onready var TimeLabel = $SidePanel/MarginContainer/TimeLeft
-onready var TimeBar = $SidePanel/MarginContainer/TimeBar
+onready var TimeBar = $SidePanel/MarginContainer2/TimeBar
+var NewTimerMax = 10
 
 #This is the start of the game
 func _ready():
-	#Makes the TimeBar fill deplete from the top
+	#Makes the TimeBar fill deplete from the top, if needed
 	#TimeBar.set_fill_mode(3)
 	_new_scene()
-	TimeBar.set_max(10)
+	TimeBar.set_max(NewTimerMax)
+	
 
 func _new_scene():
-	TimeBar.set_max(5)#GameTimer.wait_time)
+	TimeBar.set_value(5)#GameTimer.wait_time)
+	TimeBar.set_max(NewTimerMax)
+	load("res://Game Assets/UserInterface/Scenes/Stopwatch.tscn")
+	
+
+func game_cleared():
+	print("You cleared a game")
+	NewTimerMax -= 1
 	
 
 func _process(_delta):
-	TimeLabel.text = str(round(GameTimer.time_left))
-	TimeBar.value = GameTimer.time_left
+	TimeLabel.set_text(str(round(GameTimer.time_left)))
+	TimeBar.set_value(GameTimer.time_left)
 
 
 func _fail():
