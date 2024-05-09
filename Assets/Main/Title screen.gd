46043b074556d37caaced589af6568063538ec7d
Hiro Kahara <142524086+HiroKahara@users.noreply.github.com>
2024-05-09 10:38:52 -0600

    Removed unused fonts
    Also renamed multiple assets and folders. Moved the Title screen, credits, and game scenes and assets to the Games folder which is now named Assets.

---
+++ Assets/Main/Title screen.gd
@@ -0,0 +1,21 @@
+extends Control
+
+export var total_fade: NodePath
+
+func _ready():
+	$TotalFade.visible = true
+
+func _process(delta):
+	if $TotalFade.modulate.a > 0:
+		$TotalFade.modulate.a -= delta
+	else:
+		$TotalFade.visible = false
+
+func _on_Play_pressed() -> void:
+	if get_tree().change_scene("res://Assets/Main/GameScene.tscn") != OK:
+		print("An unexpected error occured when trying to switch to the title screen scene")
+func _on_Credits_pressed() -> void:
+	if get_tree().change_scene("res://Assets/Main/Credits.tscn") != OK:
+		print("An unexpected error occured when trying to switch to the title screen scene")
+func _on_Quit_Game_pressed():
+	get_tree().quit()
